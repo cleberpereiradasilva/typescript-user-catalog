c591ef5306e7ee93b4d9b709460b86ea42d6dfe8
Cleber Silva <cleber_pesilva@yahoo.com.br>
2022-12-27 21:57:26 -0300

    test: add required fields

--- src/domain/controller/login/signup-controller.spec.ts
+++ src/domain/controller/login/signup-controller.spec.ts
@@ -1,21 +1,13 @@
 import { SignupController } from "./signup-controller";
 
-describe('Test signup Controller', () => {
-    it('Should return status 400 when name is not provided', () => {
-        const sutSignupController = new SignupController();
-        const requestData: any = {
-            body: {
-                'email':'valid_email',
-                'password': 'valid_password',
-                'confirmation': 'valid_password'
-            }
-        }
-        const response = sutSignupController.handler(requestData);
-        expect(response.statusCode).toBe(400)
-    });
+const makeSutSigunupController = (): SignupController => {
+    return new SignupController()
+}
 
+
+describe('Test signup Controller', () => {
     it('Should return status 200 when name is provided', () => {
-        const sutSignupController = new SignupController();
+        const sutSignupController = makeSutSigunupController();
         const requestData: any = {
             body: {
                 'name': 'valid_name',
@@ -27,5 +19,78 @@ describe('Test signup Controller', () => {
         const response = sutSignupController.handler(requestData);
         expect(response.statusCode).toBe(200)
     });
+
+    it('Should return status 400 when name is not provided', () => {
+        const sutSignupController = makeSutSigunupController();
+        const requestData: any = {
+            body: {
+                'email':'valid_email',
+                'password': 'valid_password',
+                'confirmation': 'valid_password'
+            }
+        }
+        const response = sutSignupController.handler(requestData);
+        expect(response.statusCode).toBe(400);
+        expect(response.message).toBe(`Parameter 'name' is required`)
+    });
+
+    it('Should return status 400 when email is not provided', () => {
+        const sutSignupController = makeSutSigunupController();
+        const requestData: any = {
+            body: {
+                'name':'valid_name',
+                'password': 'valid_password',
+                'confirmation': 'valid_password'
+            }
+        }
+        const response = sutSignupController.handler(requestData);
+        expect(response.statusCode).toBe(400);
+        expect(response.message).toBe(`Parameter 'email' is required`)
+    });
+
+    it('Should return status 400 when password is not provided', () => {
+        const sutSignupController = makeSutSigunupController();
+        const requestData: any = {
+            body: {
+                'name':'valid_name',
+                'email':'valid_email',
+                'confirmation': 'valid_password'
+            }
+        }
+        const response = sutSignupController.handler(requestData);
+        expect(response.statusCode).toBe(400);
+        expect(response.message).toBe(`Parameter 'password' is required`)
+    });
+
+    it('Should return status 400 when confirmation is not provided', () => {
+        const sutSignupController = makeSutSigunupController();
+        const requestData: any = {
+            body: {
+                'name':'valid_name',
+                'email':'valid_email',
+                'password': 'valid_password'
+            }
+        }
+        const response = sutSignupController.handler(requestData);
+        expect(response.statusCode).toBe(400);
+        expect(response.message).toBe(`Parameter 'confirmation' is required`)
+    });
+
+    it('Should return status 400 when confirmation is diferent to password', () => {
+        const sutSignupController = makeSutSigunupController();
+        const requestData: any = {
+            body: {
+                'name':'valid_name',
+                'email':'valid_email',
+                'password': 'valid_password',
+                'confirmation': 'invvalid_password'
+            }
+        }
+        const response = sutSignupController.handler(requestData);
+        expect(response.statusCode).toBe(400);
+        expect(response.message).toBe(`Parameter 'confirmation' is invalid`)
+    });
+
+    
 
 });
