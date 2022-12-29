6194440f0ae9f170250a7b6d706fcf7906e360cd
Cleber Silva <cleber_pesilva@yahoo.com.br>
2022-12-29 20:48:26 -0300

    test: add routes config and set signup route

---
+++ src/external/server/express/routes/add-account.routes.test.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import request from 'supertest'
+import app  from '../';
+
+describe('AddAccount route test', () => {
+    it('should create a account using correct route', async () => {
+        
+        await request(app)
+            .post('/api/signup')           
+            .send({
+                name: 'valid_name', 
+                email: 'valid_email',
+                password: 'valid_password',
+                confirm: 'valid_password'
+            })
+            .expect(200)
+    });
+});
