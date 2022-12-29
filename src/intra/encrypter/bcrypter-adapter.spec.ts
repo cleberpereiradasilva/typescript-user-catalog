5bc847c418ac82b80d12d969e25d97e6d6a78328
Cleber Silva <cleber_pesilva@yahoo.com.br>
2022-12-28 23:16:49 -0300

    test: throw on bcrypt thow

--- src/intra/encrypter/bcrypter-adapter.spec.ts
+++ src/intra/encrypter/bcrypter-adapter.spec.ts
@@ -4,13 +4,33 @@ import { BcrypterAdapter } from "./bcrypter-adapter";
 
 describe('Bcrypter Adapter', () => {
     it('should call bcrypter with correct password', async () => {
-        
         const sutBcrypterAdapter = new BcrypterAdapter();
         const bcryptSpy = jest.spyOn(bcrypt, 'hash');
 
         await sutBcrypterAdapter.encrypt('valid_password');
 
         expect(bcryptSpy).toBeCalledWith('valid_password', 12)
+        
+    });
+
+    it('should bcrypter return a hash on success', async () => {
+        const sutBcrypterAdapter = new BcrypterAdapter();
+        jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => Promise.resolve('hash'))
+
+        const hashedValue = await sutBcrypterAdapter.encrypt('valid_password');
+
+        expect(hashedValue).toBe('hash')
+        
+    });
+
+
+    it('should throw if bcrypter throw', async () => {
+        const sutBcrypterAdapter = new BcrypterAdapter();
+        jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => Promise.reject(new Error()))
+
+        const resultThrow = sutBcrypterAdapter.encrypt('valid_password');
+
+        await expect(resultThrow).rejects.toThrow()
         
     });
 });
