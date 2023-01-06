351568c4280724a78a984fd93ef707b840af71e3
Cleber Silva <cleber_pesilva@yahoo.com.br>
2023-01-06 08:09:15 -0300

    test: ensure Decrypter will be called

--- src/data/usecase/account/db-sign-in.spec.ts
+++ src/data/usecase/account/db-sign-in.spec.ts
@@ -1,26 +1,46 @@
 import { AccountModel } from "../../../domain/usecase/model";
-import { GetAccountRepository, GetData } from "./protocols";
+import { Decrypter, GetAccountRepository, GetData } from "./protocols";
 import { DbSignIn } from './db-sign-in'
 import { SignIn } from "../../../domain/usecase/account";
 import { SignInData } from "../../../domain/usecase/account/type";
 
 type TypeSut={
     sutDbSignIn: SignIn,
-    getAccountRepositorySub: GetAccountRepository
+    getAccountRepositorySub: GetAccountRepository,
+    decrypterStub: Decrypter,
+    resolvedValues: AccountModel
 }
 const makeSut = (): TypeSut => {
+
+    class DecrypterStub implements Decrypter{
+        compare = (value: string, hash: string): Promise<boolean> => Promise.resolve(true)
+    }
     class GetAccountRepositorySub implements GetAccountRepository{
         getAccount = (getData: GetData): Promise<AccountModel | null> => {
             return Promise.resolve(null)
         }
     }
-
+    const decrypterStub = new DecrypterStub()
     const getAccountRepositorySub = new GetAccountRepositorySub()
-    const sutDbSignIn = new DbSignIn(getAccountRepositorySub)
+    const sutDbSignIn = new DbSignIn(getAccountRepositorySub, decrypterStub)
+    const resolvedValues = {
+        id: 1,
+        uuid: 'valid_uuid',
+        name: 'valid_name',
+        email: 'valid_email@email.com',
+        password: 'encrypted_password',
+        groups: [{
+            id: 1,
+            uuid: 'valid_uuid',
+            description: 'valid_description',
+        }]
+    }
 
     return {
         sutDbSignIn,
-        getAccountRepositorySub
+        getAccountRepositorySub,
+        decrypterStub,
+        resolvedValues
     }
 }
 
@@ -34,6 +54,31 @@ describe('SignIn data account', () => {
         }
         await sutDbSignIn.login(signInData)
         expect(getSpy).toBeCalledWith({field: 'email', value: signInData.email})       
-
     });
+
+    it('Should return null when account not found', async () => {
+        const {sutDbSignIn, getAccountRepositorySub }  = makeSut()
+        jest.spyOn(getAccountRepositorySub, 'getAccount').mockResolvedValue(null)
+        const signInData: SignInData = {
+            email: 'valid_email@email.com',
+            password: 'valid_password',
+        }
+        const isValid = await sutDbSignIn.login(signInData)
+        expect(isValid).toBe(null)
+    });
+
+    it('Should call decrypter with correct values', async () => {
+        const {sutDbSignIn, decrypterStub, getAccountRepositorySub, resolvedValues }  = makeSut()
+        jest.spyOn(getAccountRepositorySub, 'getAccount').mockResolvedValue(resolvedValues)
+
+        const compareSpy = jest.spyOn(decrypterStub, 'compare')
+        const signInData: SignInData = {
+            email: 'valid_email@email.com',
+            password: 'valid_password',
+        }
+        await sutDbSignIn.login(signInData)
+        expect(compareSpy).toBeCalledWith(signInData.password, 'encrypted_password')       
+    });
+
+
 });
