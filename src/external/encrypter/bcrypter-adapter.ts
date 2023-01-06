39964f8a88c0a71f5cf4ef0af64b1bb88c8a59cb
Cleber Silva <cleber_pesilva@yahoo.com.br>
2023-01-06 08:50:49 -0300

    feat: create decrypt in bcrypt-adapter

--- src/external/encrypter/bcrypter-adapter.ts
+++ src/external/encrypter/bcrypter-adapter.ts
@@ -1,8 +1,12 @@
 import bcrypt from 'bcrypt'
-import { Encrypter } from "../../data/usecase/account/protocols";
+import { Decrypter, Encrypter } from "../../data/usecase/account/protocols";
 
-export class BcrypterAdapter implements Encrypter{
+export class BcrypterAdapter implements Encrypter, Decrypter{
     encrypt = (value: string): Promise<string> =>{
         return bcrypt.hash(value, 12)
+    }
+
+    compare = (value: string, hash: string): Promise<boolean> => {
+        return bcrypt.compare(value, hash)
     }
 }
