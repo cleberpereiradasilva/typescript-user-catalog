e85a5d8ed4dc913da4987cd215d11f9384bc7240
Cleber Silva <cleber_pesilva@yahoo.com.br>
2023-01-06 08:38:00 -0300

    feat: return a valid token when sucess in login

--- src/data/usecase/account/db-sign-in.ts
+++ src/data/usecase/account/db-sign-in.ts
@@ -1,21 +1,25 @@
 import { SignIn } from "../../../domain/usecase/account";
 import { SignInData } from "../../../domain/usecase/account/type";
-import { AccountModel } from "../../../domain/usecase/model";
-import { Decrypter, GetAccountRepository } from "./protocols";
+import { Decrypter, Encrypter, GetAccountRepository } from "./protocols";
 
 export class DbSignIn implements SignIn{
     constructor(
         private readonly getAccountRepository: GetAccountRepository,
-        private readonly decrypter: Decrypter
+        private readonly decrypter: Decrypter,
+        private readonly encrypter: Encrypter
     ){}
     
-    login = async (signInData: SignInData): Promise<AccountModel | null> => {
+    login = async (signInData: SignInData): Promise<string | null> => {
         const account = await this.getAccountRepository.getAccount({field: 'email', value: signInData.email})
         if(!account){
             return null
         }
-        await this.decrypter.compare(signInData.password, account.password )
-        return null
+        const isValid = await this.decrypter.compare(signInData.password, account.password )
+        if(!isValid){
+            return null
+        }
+
+        return this.encrypter.encrypt(account.uuid)
     }
 
 }
--- src/domain/usecase/account/sign-in.ts
+++ src/domain/usecase/account/sign-in.ts
@@ -1,5 +1,5 @@
 import { AccountModel } from "../model";
 import { SignInData } from "./type";
 export interface SignIn{
-    login: (signInData: SignInData) => Promise<AccountModel | null>
+    login: (signInData: SignInData) => Promise<String | null>
 }
