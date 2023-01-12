9dcc7a97feece9c53c73d41cd84732b6dd4286f9
Cleber Silva <cleber_pesilva@yahoo.com.br>
2023-01-12 07:56:28 -0300

    test: sign-in router

--- src/external/server/express/routes/sign-in.routes.test.ts
+++ src/external/server/express/routes/sign-in.routes.test.ts
@@ -1,27 +1,58 @@
 import request from 'supertest'
+import { faker } from '@faker-js/faker';
 import app  from '../';
+import { AccountData } from '../../../../domain/usecase/account/type';
 import { Controller } from '../../../../presentation/controller/protocols/interface';
 import { HttpRequest, HttpResponse } from '../../../../presentation/controller/protocols/types';
-import { httpResponseOk } from '../../../../presentation/controller/helper/http-response';
-class MakeSigInControllerStub implements Controller{
+import { httpResponseOk, httpUnAuthorized } from '../../../../presentation/controller/helper/http-response';
+
+const accountData: AccountData = {
+    name: faker.name.firstName(),
+    email: faker.internet.email(),
+    password:  faker.internet.password()
+}
+class MakeSignInControllerStub implements Controller{
     handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-        return httpResponseOk('')
+        return httpResponseOk(
+            {
+                id: 1,
+                uuid: 'valid_uuid',
+                groups:[],
+                ...accountData
+            })
     }
 }
 
-const makeSigInControllerStub = new MakeSigInControllerStub()
+const makeSignInControllerStub = new MakeSignInControllerStub()
+
 jest.mock('../../../../main/factories/sign-in.ts', () => ({
-    makeSignInController: () => makeSigInControllerStub
+    makeSignInController: () => {
+        const signInController = makeSignInControllerStub
+        return signInController
+    }
 }))
 
+
 describe('Sign in route test', () => {
-    it('should sign in using correct route', async () => {
+    jest.spyOn(makeSignInControllerStub, 'handle').mockImplementationOnce(() => httpUnAuthorized())
+    it('should return unauthorized when pass wrong token', async () => {
         await request(app)
             .post('/api/sign-in')           
             .send({
                 email: 'valid_email',
                 password: 'valid_password'
             })
+            .expect(401)
+    });
+
+    it('should sign in using correct route and email and password', async () => {
+        await request(app)
+            .post('/api/sign-in')           
+            .send({
+                email: accountData.email,
+                password:  accountData.password
+            })
             .expect(200)
+        
     });
 });
