13feeb365ab53ebd1103c4b2e7cbe220ffe2c6dd
Cleber Silva <cleber_pesilva@yahoo.com.br>
2023-01-17 09:31:31 -0300

    test: check if account exist in AuthMiddleware

--- src/presentation/controller/middleware/auth-middleware.spec.ts
+++ src/presentation/controller/middleware/auth-middleware.spec.ts
@@ -1,4 +1,4 @@
-import { httpForbidden } from '../helper/http-response';
+import { httpForbidden, httpResponseOk } from '../helper/http-response';
 import { AuthMiddleware } from './auth-middleware'
 import { GetAccountByToken } from '../../../domain/usecase/account'
 import { AccountModel } from '../../../domain/usecase/model';
@@ -32,20 +32,19 @@ const makeSut = (): SutTypes => {
         sutAuthMiddleware,
         getAccountByTokenStub,
     }
-
 }
 
 describe('Auth Middleware', () => {
-    it('should return httpForbidden(403) if token in header is not provider', () => {
+    it('should return httpForbidden(403) if token in header is not provider', async () => {
         const { sutAuthMiddleware } = makeSut()
         const httpRequest = {
             headers: {}
         }
-        const response = sutAuthMiddleware.handle(httpRequest)
-        expect(response).toEqual(httpForbidden())
+        const response = await sutAuthMiddleware.handle(httpRequest)
+        expect(response).toEqual(await httpForbidden())
     });
 
-    it('should call getAccountByToken with correct token', () => {
+    it('should call getAccountByToken with correct token', async () => {
         const { sutAuthMiddleware, getAccountByTokenStub } = makeSut()
         const getAccountSpy = jest.spyOn(getAccountByTokenStub, 'getAccount')
         const httpRequest = {
@@ -53,9 +52,20 @@ describe('Auth Middleware', () => {
                 'x-access-token': 'valid_token'
             }
         }
-        sutAuthMiddleware.handle(httpRequest)
+        await sutAuthMiddleware.handle(httpRequest)
         expect(getAccountSpy).toBeCalledWith('valid_token')
     });
 
+    it('should return httpForbidden(403) when can not find a account', async () => {
+        const { sutAuthMiddleware, getAccountByTokenStub } = makeSut()
+        jest.spyOn(getAccountByTokenStub, 'getAccount').mockImplementationOnce(() => Promise.resolve(null))
+        const httpRequest = {
+            headers: {
+                'x-access-token': 'in_valid_token'
+            }
+        }
+        const response = await sutAuthMiddleware.handle(httpRequest)
+        expect(response).toEqual(await httpForbidden())
+    });
 
 });
