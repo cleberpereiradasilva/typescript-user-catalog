88fc885b95d8f0127c55d5886a20c6127984d803
Cleber Silva <cleber_pesilva@yahoo.com.br>
2023-01-17 23:26:13 -0300

    feat: should return null if Decrypter return null

--- src/data/usecase/account/db-get-account-by-token.ts
+++ src/data/usecase/account/db-get-account-by-token.ts
@@ -5,7 +5,10 @@ import { Decrypter } from "./protocols";
 export class DbGetAccountByToken implements GetAccountByToken{
     constructor(private readonly decrypter: Decrypter){}
     getAccount = async (token: string):Promise<AccountModel> => {
-        await this.decrypter.descrypt(token)
-        return Promise.resolve(null)
+        const tokenObject = await this.decrypter.descrypt(token)
+        if(tokenObject){
+            return tokenObject as AccountModel
+        }
+        return null
     }
 }
