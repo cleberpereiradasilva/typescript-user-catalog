7d73d4c75eca09ee78314fa392f16200808dda24
Cleber Silva <cleber_pesilva@yahoo.com.br>
2023-01-17 22:00:18 -0300

    feat: return throw when getAccountByToken throw

--- src/presentation/controller/middleware/auth-middleware.ts
+++ src/presentation/controller/middleware/auth-middleware.ts
@@ -1,20 +1,25 @@
 import { GetAccountByToken } from "../../../domain/usecase/account";
-import { httpForbidden, httpResponseOk } from "../helper/http-response";
+import { httpForbidden, httpResponseOk, httpServerError } from "../helper/http-response";
 import { Controller } from "../protocols/interface";
 import { HttpRequest, HttpResponse } from "../protocols/types";
 
 export class AuthMiddleware implements Controller{
     constructor(private readonly getAccountByToken: GetAccountByToken){}
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-        if(!httpRequest.headers['x-access-token']){
-            return httpForbidden()
+        try{
+            if(!httpRequest.headers['x-access-token']){
+                return httpForbidden()
+            }
+            const account = await this.getAccountByToken.getAccount(httpRequest.headers['x-access-token'])
+            
+            if(!account){
+                return httpForbidden()
+            }
+            return httpResponseOk(account)
+            
+        }catch(error){
+            return httpServerError()
         }
-        const account = await this.getAccountByToken.getAccount(httpRequest.headers['x-access-token'])
-        
-        if(!account){
-           return Promise.resolve(httpForbidden()) 
-        }
-        return Promise.resolve(httpResponseOk({}))
     }
 
 }
